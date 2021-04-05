1d60f30315f766caac310a218edf62c81b712ea3
Wilkson Carlos <wilkson.carlos@outlook.com>
2021-04-05 10:44:05 -0300

    first commit

---
+++ SalesMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
